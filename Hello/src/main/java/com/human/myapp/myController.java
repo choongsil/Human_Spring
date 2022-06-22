3eda3fe4aef64504b3c6ee6358e48d66747635ba
choongsil <s.c.sil35@gmail.com>
2022-06-22 11:30:46 +0900

    사칙연산

--- Hello/src/main/java/com/human/myapp/myController.java
+++ Hello/src/main/java/com/human/myapp/myController.java
@@ -21,4 +21,29 @@ public class myController {
 
 		return "nick";
 	}
+
+	@RequestMapping(value = "/calc", method = RequestMethod.GET)
+	public String doCalc(HttpServletRequest req, Model model) {
+		String str1 = req.getParameter("op");
+		String str2 = req.getParameter("x");
+		String str3 = req.getParameter("y");
+		if (str1 == null || str1.equals("") || str2 == null || str2.equals("") || str3 == null || str3.equals("")) {
+			model.addAttribute("title", "오류상황");
+			model.addAttribute("result", "유효한 값이 주어지지 않았습니다.");
+
+		} else if (str1.equals("plus")) {
+			model.addAttribute("title", "더하기");
+			model.addAttribute("result", Integer.parseInt(str2) + Integer.parseInt(str3));
+		} else if (str1.equals("minus")) {
+			model.addAttribute("title", "빼기");
+			model.addAttribute("result", Integer.parseInt(str2) - Integer.parseInt(str3));
+		}else if(str1.equals("divided")) {
+			model.addAttribute("title", "나누기");
+			model.addAttribute("result", Integer.parseInt(str2) / Integer.parseInt(str3));
+		}else if(str1.equals("multyply")) {
+			model.addAttribute("title", "곱하기");
+			model.addAttribute("result", Integer.parseInt(str2) * Integer.parseInt(str3));
+		}
+		return "result";
+	}
 }
