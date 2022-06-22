b186b9b8263647b472ce99289fc7ff2efea61b1e
choongsil <s.c.sil35@gmail.com>
2022-06-22 16:53:17 +0900

    REST ARI/URL 사용

--- Hello/src/main/java/com/human/myapp/myController.java
+++ Hello/src/main/java/com/human/myapp/myController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,12 +22,12 @@ public class myController {
 
 		return "nick";
 	}
-
-	@RequestMapping(value = "/calc", method = RequestMethod.GET)
-	public String doCalc(HttpServletRequest req, Model model) {
-		String str1 = req.getParameter("op");
-		String str2 = req.getParameter("x");
-		String str3 = req.getParameter("y");
+//@PathVariable 사용
+	@RequestMapping("/calc/{str1}/{str2}/{str3}")
+	public String doCalc(@PathVariable String str1, @PathVariable String str2, @PathVariable String str3, Model model) {
+//		String str1 = req.getParameter("op");
+//		String str2 = req.getParameter("x");
+//		String str3 = req.getParameter("y");
 		if (str1 == null || str1.equals("") || str2 == null || str2.equals("") || str3 == null || str3.equals("")) {
 			model.addAttribute("title", "오류상황");
 			model.addAttribute("result", "유효한 값이 주어지지 않았습니다.");
@@ -49,53 +50,53 @@ public class myController {
 		return "result";
 
 	}
+
 	@RequestMapping(value = "/computer", method = RequestMethod.GET)
 	public String dosolve(HttpServletRequest req, Model model) {
 		String str1 = req.getParameter("x");
 		String str2 = req.getParameter("y");
-		int x= Integer.parseInt(str1);
-		int y= Integer.parseInt(str2);
-		if(x-y>0) {
+		int x = Integer.parseInt(str1);
+		int y = Integer.parseInt(str2);
+		if (x - y > 0) {
 			model.addAttribute("x", x);
 			model.addAttribute("y", y);
-			model.addAttribute("result", x-y);
+			model.addAttribute("result", x - y);
 			return "on";
-		}else {
+		} else {
 			model.addAttribute("x", x);
 			model.addAttribute("y", y);
-			model.addAttribute("result", x-y);
+			model.addAttribute("result", x - y);
 			return "off";
 		}
 	}
-	//회원가입 로직
+
+	// 회원가입 로직
 	@RequestMapping("/login")
 	public String doLogin() {
 		return "login";
 	}
+
 	@RequestMapping("/user_chek")
 	public String doUserCheck(HttpServletRequest req, Model model) {
-		String user_id=req.getParameter("userid");
-		String password=req.getParameter("pwd");
-		if(user_id.equals("human")&&password.equals("human123")) {
-			model.addAttribute("userid",user_id);
-			model.addAttribute("pwd",password);
+		String user_id = req.getParameter("userid");
+		String password = req.getParameter("pwd");
+		if (user_id.equals("human") && password.equals("human123")) {
+			model.addAttribute("userid", user_id);
+			model.addAttribute("pwd", password);
 			return "userinfo";
-		}else {
-			model.addAttribute("userid",user_id);
+		} else {
+			model.addAttribute("userid", user_id);
 			return "signin";
 		}
-		
+
 	}
+
 	@RequestMapping("/user_siginin")
 	public String doSignin(HttpServletRequest req, Model model) {
-		String uid=req.getParameter("userid");
-		String pwd=req.getParameter("pwd");
-		String pwd1=req.getParameter("pwd1");
-		if(!pwd.equals(pwd1)) {
-			model.addAttribute("userid",uid);
-			return "signin";
-		}else {
-			return "redirect:/login";
-		}
+		String uid = req.getParameter("userid");
+		String pwd = req.getParameter("pwd");
+		String pwd1 = req.getParameter("pwd1");
+		return "redirect:/login";
+
 	}
 }
