6135dc9bc440e2526dba9ffc9c36648ada466b0f
choongsil <s.c.sil35@gmail.com>
2022-06-27 10:00:12 +0900

    DB에서 원하는 데이터만 가져오기

--- BBC/src/main/java/com/human/myapp/iEmployee.java
+++ BBC/src/main/java/com/human/myapp/iEmployee.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public interface iEmployee {
 	ArrayList<empDTO> listEmployees();
+	Member getMember(int id);
 	void addEmployee(String ename, int eid, int mid);
 	void removeEmployee(int eid);
 }
